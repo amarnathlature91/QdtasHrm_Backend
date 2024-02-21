bcdbe915671d3ef7172ed354de64ef7049029561
amarlature <amarlature7721@gmail.com>
2024-02-22 02:46:37 +0530

    updated postgreSQL configs

--- src/main/java/com/qdtas/entity/User.java
+++ src/main/java/com/qdtas/entity/User.java
@@ -12,6 +12,7 @@ import java.util.Date;
 @AllArgsConstructor
 @NoArgsConstructor
 @Entity
+@Table(name = "users")
 public class User{
     @Id
     @GeneratedValue(strategy= GenerationType.IDENTITY)
