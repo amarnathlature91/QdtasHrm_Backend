c935ba6096f5d251964ec2d4cee3451b1f8acf85
amarlature <amarlature7721@gmail.com>
2024-03-04 18:13:05 +0530

    -Added Project Validations

--- src/main/java/com/qdtas/dto/ProjectDTO.java
+++ src/main/java/com/qdtas/dto/ProjectDTO.java
@@ -1,5 +1,7 @@
 package com.qdtas.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
@@ -9,17 +11,27 @@ import lombok.*;
 @NoArgsConstructor
 public class ProjectDTO {
 
+    @NotBlank(message = "Project Name Cannot be Blank")
+    @NotNull(message = "Project Name Cannot be Blank")
     @Pattern(regexp = "^[a-zA-Z ]+$", message = "Only alphabets are allowed")
     private String projectName;
 
+    @NotBlank(message = "Project Description Cannot be Blank")
+    @NotNull(message = "Project Description Cannot be Blank")
     private  String description;
 
+    @NotBlank(message = "Project Status Cannot be Blank")
+    @NotNull(message = "Project Status Cannot be Blank")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Only alphabets are allowed")
     private String status;
 
+    @NotBlank(message = "Project Type Cannot be Blank")
+    @NotNull(message = "Project Type Cannot be Blank")
     @Pattern(regexp = "^[a-zA-Z]+$", message = "Only alphabets are allowed")
     private String type;
 
+    @NotBlank(message = "Client Cannot be Blank")
+    @NotNull(message = "Client Cannot be Blank")
     private String client;
 
 }
